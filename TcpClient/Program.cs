63f8e05bc222e53985c419c638b4d7b3364aeab1
Sashs <sashasam348@gmail.com>
2023-02-16 22:01:06 +0200

    Added client from task

--- TcpClient/Program.cs
+++ TcpClient/Program.cs
@@ -1,3 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
-Console.WriteLine("Hello, World!");
+try
+{
+    SendMessageFromSocket(54739);
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
+finally
+{
+    Console.ReadLine();
+}
+
+
+void SendMessageFromSocket(int port)
+{
+    Console.OutputEncoding = Encoding.Unicode;
+    Console.InputEncoding = Encoding.Unicode;
+    var bytes = new byte[1024];
+
+    var ipHost = Dns.GetHostEntry("localhost");
+    var ipAddr = ipHost.AddressList[0];
+    var ipEndPoint = new IPEndPoint(ipAddr, port);
+    var sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+    sender.Connect(ipEndPoint);
+    Console.Write("Enter a message: ");
+    var message = Console.ReadLine();
+    Console.WriteLine($"Connected to the {sender.RemoteEndPoint}");
+    var msg = Encoding.UTF8.GetBytes(message);
+    sender.Send(msg);
+    var bytesRec = sender.Receive(bytes);
+    Console.WriteLine($"Answer: {Encoding.UTF8.GetString(bytes, 0, bytesRec)}");
+    if (message.Contains("<TheEnd>"))
+    {
+        SendMessageFromSocket(port);
+    }
+    sender.Shutdown(SocketShutdown.Both);
+    sender.Close();
+}
